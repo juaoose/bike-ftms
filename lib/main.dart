22dc65f6635dc8031d5b3fb29a833fbb03c7c947
Juan <juaoose@users.noreply.github.com>
2024-05-29 15:35:44 -0500

    chore: extract workout selection from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bike_ftms/screens/file_selection.dart';
+import 'package:bike_ftms/screens/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,15 +12,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Indoor Cyclist'),
-        ),
-        body: const Center(
-          child: FileSelectionScreen(),
-        ),
-      ),
+    return const MaterialApp(
+      home: FileSelectionScreen(),
     );
   }
 }
--- lib/screens/file_selection.dart
+++ lib/screens/file_selection.dart
@@ -1,4 +1,5 @@
 import 'package:bike_ftms/models/fit_workout.dart';
+import 'package:bike_ftms/screens/settings.dart';
 import 'package:bike_ftms/screens/workout.dart';
 import 'package:bike_ftms/static_workout_graph.dart';
 import 'package:flutter/material.dart';
@@ -12,33 +13,47 @@ class FileSelectionScreen extends ConsumerWidget {
     final fitFileState = ref.watch(fitFileProvider);
     final fitFileNotifier = ref.read(fitFileProvider.notifier);
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          onPressed: () => fitFileNotifier.pickFile(),
-          child: const Text('Pick File'),
-        ),
-        if (fitFileState.isLoading)
-          const CircularProgressIndicator()
-        else if (fitFileState.workoutName != null)
-          Text(
-              'FIT File Result: ${fitFileState.workoutName} ${fitFileState.workoutSteps?.length}')
-        else if (fitFileState.errorMessage != null)
-          Text('Error: ${fitFileState.errorMessage}'),
-        if (fitFileState.selectedFile != null)
-          StaticWorkoutGraph(state: fitFileState),
-        if (fitFileState.selectedFile != null)
-          TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const WorkoutPlayerScreen()),
-                );
-              },
-              child: const Text("Start!"))
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Workout selection'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SettingsScreen()),
+              );
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () => fitFileNotifier.pickFile(),
+            child: const Text('Pick File'),
+          ),
+          if (fitFileState.isLoading)
+            const CircularProgressIndicator()
+          else if (fitFileState.workoutName != null)
+            Text('Workout Name: ${fitFileState.workoutName}')
+          else if (fitFileState.errorMessage != null)
+            Text('Error: ${fitFileState.errorMessage}'),
+          if (fitFileState.selectedFile != null)
+            StaticWorkoutGraph(state: fitFileState),
+          if (fitFileState.selectedFile != null)
+            TextButton(
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const WorkoutPlayerScreen()),
+                    ),
+                child: const Text("Start!"))
+        ],
+      ),
     );
   }
 }
